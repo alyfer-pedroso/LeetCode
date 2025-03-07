5f71124e80d6730f95ab248075562a5d667b731a
Alyfer Pedroso <138719366+alyfer-pedroso@users.noreply.github.com>
2025-03-06 23:32:00 -0300

    Time: 1 ms (5.87%) | Memory: 57.5 MB (7.18%) - LeetSync

---
+++ 66-plus-one/plus-one.ts
@@ -0,0 +1,17 @@
+function plusOne(digits: number[]): number[] {
+  if (digits.length > 100 || digits.length < 1 || digits.every((digit) => digit > 9)) return [];
+
+  let bigDigit = BigInt(0);
+  for (let i = 0; i < digits.length; i++) {
+    bigDigit = BigInt(bigDigit * BigInt(10) + BigInt(digits[i] % 10));
+  }
+  bigDigit++;
+
+  let final_digits: number[] = [];
+  while (bigDigit > BigInt(0)) {
+    final_digits.unshift(Number(bigDigit % BigInt(10)));
+    bigDigit = BigInt(bigDigit / BigInt(10));
+  }
+
+  return final_digits;
+}

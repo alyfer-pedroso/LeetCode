27c770f30bd91fdd21bf1547c907d56a031a3ad8
Alyfer Pedroso <138719366+alyfer-pedroso@users.noreply.github.com>
2025-03-20 11:49:24 -0300

    Time: 14 ms (50.00%) | Memory: 69.6 MB (18.04%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.ts
@@ -0,0 +1,10 @@
+function containsDuplicate(nums: number[]): boolean {
+  const numSet = new Set<number>();
+  for (const num of nums) {
+    if (numSet.has(num)) {
+      return true;
+    }
+    numSet.add(num);
+  }
+  return false;
+}

55a44ae8534f3845a7e98970d6b384be536eed7d
Alyfer Pedroso <138719366+alyfer-pedroso@users.noreply.github.com>
2025-03-21 10:36:55 -0300

    Time: 1 ms (67.16%) | Memory: 58.5 MB (38.92%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.ts
@@ -0,0 +1,9 @@
+function fizzBuzz(n: number): string[] {
+  return Array.from({ length: n }, (_, idx) => {
+    const num = idx + 1;
+    if (!(num % 3) && !(num % 5)) return "FizzBuzz";
+    if (!(num % 3)) return "Fizz";
+    if (!(num % 5)) return "Buzz";
+    return num.toString();
+  });
+}

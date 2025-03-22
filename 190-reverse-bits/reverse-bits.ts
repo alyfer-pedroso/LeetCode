d16ddd6f6ef264444fb303cc556ca9f6e3f9a9be
Alyfer Pedroso <138719366+alyfer-pedroso@users.noreply.github.com>
2025-03-22 10:24:38 -0300

    Time: 65 ms (31.73%) | Memory: 56.6 MB (45.87%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.ts
@@ -0,0 +1,15 @@
+function reverseBits(n: number): number {
+  const mask1 = 0x55555555;
+  const mask2 = 0x33333333;
+  const mask3 = 0x0f0f0f0f;
+  const mask4 = 0x00ff00ff;
+  const mask5 = 0x0000ffff;
+
+  n = ((n >>> 1) & mask1) | ((n & mask1) << 1);
+  n = ((n >>> 2) & mask2) | ((n & mask2) << 2);
+  n = ((n >>> 4) & mask3) | ((n & mask3) << 4);
+  n = ((n >>> 8) & mask4) | ((n & mask4) << 8);
+  n = ((n >>> 16) & mask5) | ((n & mask5) << 16);
+
+  return n >>> 0;
+};

1626b1e4e36c6add9ea5a9790f7e890833f08acb
Alyfer Pedroso <138719366+alyfer-pedroso@users.noreply.github.com>
2025-03-25 11:58:23 -0300

    Delete 26-remove-duplicates-from-sorted-array directory

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.ts
+++
@@ -1,9 +0,0 @@
-function removeDuplicates(nums: number[]): number {
-  const length = nums.length;
-  for (let i = 0; i < length; i++) {
-    while (nums.indexOf(nums[i]) !== nums.lastIndexOf(nums[i])) {
-      nums.splice(nums.indexOf(nums[i]), 1);
-    }
-  }
-  return length;
-}

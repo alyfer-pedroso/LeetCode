9868f1e127a61cff056de7baeb997ec4de1c5a3e
Alyfer Pedroso <138719366+alyfer-pedroso@users.noreply.github.com>
2025-03-26 09:00:29 -0300

    Delete 35-search-insert-position directory

--- 35-search-insert-position/search-insert-position.ts
+++
@@ -1,6 +0,0 @@
-function searchInsert(nums: number[], target: number): number {
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === target || nums[i] > target) return i
-        if (nums[i] < target && i === nums.length - 1) return i + 1;
-    }
-};

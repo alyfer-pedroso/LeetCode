cc91f909fb514162f93653e59bbe19c56e74d908
Alyfer Pedroso <138719366+alyfer-pedroso@users.noreply.github.com>
2025-03-27 08:33:19 -0300

    Time: 0 ms (100.00%) | Memory: 60 MB (19.01%) - LeetSync

---
+++ 792-binary-search/binary-search.ts
@@ -0,0 +1,17 @@
+function search(nums: number[], target: number): number {
+    let low = 0;
+    let high = nums.length - 1;
+
+    while (low <= high) {
+        let mid = Math.floor(low + (high - low) / 2);
+
+        if (nums[mid] === target) return mid;
+        if (nums[mid] < target) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    return -1;
+};
